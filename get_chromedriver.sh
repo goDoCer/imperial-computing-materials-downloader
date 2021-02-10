809c08d5797ce799ee1c6c0e57e50c78a356fa6a
Pranav <pb719@ic.ac.uk>
2021-02-10 22:18:07 +0530

    UPDATE get_chromedriver.sh to get the latest version

--- get_chromedriver.sh
+++ get_chromedriver.sh
@@ -1,1 +1,4 @@
-wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/2.10/chromedriver_linux64.zip && sudo unzip /tmp/chromedriver.zip chromedriver -d ./lib/;
+sudo apt-get install unzip &&
+LATEST_VERSION=$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE) &&
+wget -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/$LATEST_VERSION/chromedriver_linux64.zip &&
+sudo unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/;
