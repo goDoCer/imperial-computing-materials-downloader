4c6f067fd63fd2ae7711b1d5a778f7cea12db509
Pranav <pb719@ic.ac.uk>
2021-02-10 22:18:36 +0530

    UPDATE config.py to use chromedriver from /usr/local/bin

--- lib/config.py
+++ lib/config.py
@@ -5,7 +5,7 @@ auth = json.load(open("./lib/auth.json"))
 SHORTCODE         = auth["shortcode"]
 PASSWORD          = auth["password"]
 DIRECTORY         = auth["directory"]
-CHROMEDRIVER_PATH = "./lib/chromedriver"
+CHROMEDRIVER_PATH = "/usr/local/bin/chromedriver"
 
 MATERIALS_URL     = "https://materials.doc.ic.ac.uk/"
 YEAR              = 2021
